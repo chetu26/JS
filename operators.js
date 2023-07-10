ada82d33e9cd1484dc64c5ad2ceaafcce94fbaa4
chetna <guptachetna96@gmail.com>
2023-07-10 10:31:18 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,39 @@
+// Arithmetic Operators
+let a = 10;
+let b = 5;
+
+console.log(a + b); // Addition
+console.log(a - b); // Subtraction
+console.log(a * b); // Multiplication
+console.log(a / b); // Division
+console.log(a % b); // Modulo
+
+// Comparison Operators
+console.log(a > b); // Greater than
+console.log(a < b); // Less than
+console.log(a >= b); // Greater than or equal to
+console.log(a <= b); // Less than or equal to
+console.log(a === b); // Equal to (strict equality)
+console.log(a !== b); // Not equal to (strict inequality)
+
+// Logical (Relational) Operators
+let x = true;
+let y = false;
+
+console.log(x && y); // Logical AND
+console.log(x || y); // Logical OR
+console.log(!x); // Logical NOT
+
+// Assignment Operators
+let c = 15;
+c += 5; // c = c + 5
+console.log(c);
+
+let d = 10;
+d -= 3; // d = d - 3
+console.log(d);
+
+// Conditional (Ternary) Operator
+let age = 18;
+let isAdult = (age >= 18) ? 'Yes' : 'No';
+console.log(isAdult);
