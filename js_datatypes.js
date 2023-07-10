2ca2e78d44f94e97bcbf5d1307d7ca210b0cc689
chetna <guptachetna96@gmail.com>
2023-07-10 10:23:05 +0530

    js datatypes example

---
+++ js_datatypes.js
@@ -0,0 +1,45 @@
+// Primitive Data Types
+// Numbers
+let num = 10;
+console.log(typeof num); // Output: number
+
+// String
+let str = 'Hello';
+console.log(typeof str); // Output: string
+
+// Boolean
+let bool = true;
+console.log(typeof bool); // Output: boolean
+
+// Trivial Data Types
+// null
+let nullValue = null;
+console.log(typeof nullValue); // Output: object
+
+// undefined
+let undefinedValue;
+console.log(typeof undefinedValue); // Output: undefined
+
+// Composite Data Type
+// Object
+let person = {
+  name: 'John',
+  age: 25,
+  isStudent: true,
+};
+console.log(typeof person); // Output: object
+
+// Objects in JavaScript
+console.log(typeof Math); // Output: object
+console.log(typeof Date); // Output: function (special type of object)
+console.log(typeof RegExp); // Output: function (special type of object)
+
+// Numbers, Boolean, Strings, Arrays, Date, Math, RegExp are all objects too
+console.log(typeof Number); // Output: function (special type of object)
+console.log(typeof Boolean); // Output: function (special type of object)
+console.log(typeof String); // Output: function (special type of object)
+console.log(typeof Array); // Output: function (special type of object)
+console.log(typeof Date); // Output: function (special type of object)
+console.log(typeof Math); // Output: object
+console.log(typeof RegExp); // Output: function (special type of object)
+console.log(typeof document); // Output: object (part of HTML DOM)
